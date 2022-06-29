1f334227acf0c7bc29bdb64e4fbb43dd92ef0a56
Aman Gupta <101828125+Aman-gupta98@users.noreply.github.com>
2022-06-29 13:03:20 +0530

    Add files via upload

---
+++ Arithmatic_operator.java
@@ -0,0 +1,14 @@
+public class Arithmatic_operator {
+	public static void add()
+	{
+		int a=10;
+		float b=20.3f;
+		float addition= a+b;
+		System.out.println(addition);
+	}
+	public static void main(String args[])
+	{
+		System.out.println("Hello you are in arithmatic class");
+		add();
+	}	
+}
